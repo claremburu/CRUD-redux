67466f1c2b6737ad5528a2fa4ead5458c5185834
claremburu <mburuclare8@gmail.com>
2022-06-05 05:50:02 +0300

    create react-crud

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import usersReducer from "./features/Users";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: {
+    users: usersReducer
+  }
+});
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
